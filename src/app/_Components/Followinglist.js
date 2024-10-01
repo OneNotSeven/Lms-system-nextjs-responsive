1a876dced479ed7be8e5bd35573aab6c3065a5e5
Aman <160247525+OneNotSeven@users.noreply.github.com>
2024-10-01 23:30:21 +0530

    Update Followinglist.js

--- src/app/_Components/Followinglist.js
+++ src/app/_Components/Followinglist.js
@@ -165,7 +165,7 @@ const Followinglist = () => {
               <div className="p-6">
                         <h2 className="text-xl font-semibold capitalize text-[#d42eeb] mb-2">{items?.title}</h2>
                         <div className="flex items-center mb-4">
-                  <svg className=" w-6 h-6 text-[#e64cfb] dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                  <svg className=" w-6 h-6 text-[#e64cfb]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
   <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m7 16 4-4-4-4m6 8 4-4-4-4"/>
 </svg>
 
