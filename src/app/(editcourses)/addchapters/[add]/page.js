989ae9e22f59532ca45bff444f9cdf2c59899e42
Aman <160247525+OneNotSeven@users.noreply.github.com>
2024-10-01 23:26:20 +0530

    Update page.js

--- src/app/(editcourses)/addchapters/[add]/page.js
+++ src/app/(editcourses)/addchapters/[add]/page.js
@@ -204,11 +204,11 @@ setloader(true)
       <div className="w-full p-7 flex flex-col font-[Poppins]">
         <h5
           id="drawer-label"
-          className="inline-flex w-full gap-3 items-center mb-6 text-base font-semibold text-gray-500 uppercase dark:text-gray-400"
+          className="inline-flex w-full gap-3 items-center mb-6 text-base font-semibold text-gray-500 uppercase"
         >
           <Link href="/teachers/upload">
             <svg
-              className="w-6 h-6 text-gray-800 dark:text-white"
+              className="w-6 h-6 text-gray-800"
               aria-hidden="true"
               xmlns="http://www.w3.org/2000/svg"
               width="24"
@@ -232,7 +232,7 @@ setloader(true)
             <div className="mb-6">
               <label
                 for="chaptername"
-                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                className="block mb-2 text-sm font-medium text-gray-900"
               >
                 Chapter name
               </label>
@@ -241,7 +241,7 @@ setloader(true)
                   id="chaptername"
                   value={values.chaptername}
                   name="chaptername"
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
                 placeholder="Chapter 1: Gravity"
                 required
                 onChange= {handleChange}
@@ -255,7 +255,7 @@ setloader(true)
             <div className="mb-6">
               <label
                 for="title"
-                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                className="block mb-2 text-sm font-medium text-gray-900"
               >
                 Title
               </label>
@@ -264,7 +264,7 @@ setloader(true)
                   id="title"
                   name="title"
                   value={values.title}
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
                 placeholder="Gravity"
                 required
                 onChange= {handleChange}
@@ -279,7 +279,7 @@ setloader(true)
             <div className="mb-6">
               <label
                 for="description"
-                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                className="block mb-2 text-sm font-medium text-gray-900"
               >
                 Description
               </label>
@@ -288,7 +288,7 @@ setloader(true)
                   rows="4"
                   name="desc"
                   value={values.desc}
-                className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
                   placeholder="Write a description..."
                   onChange= {handleChange}
                 onBlur={handleBlur}
@@ -324,7 +324,7 @@ setloader(true)
                       d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
                     />
                   </svg>
-                  <p className="text-sm font-semibold text-gray-400 dark:text-gray-400">
+                  <p className="text-sm font-semibold text-gray-400">
                     Click to upload
                   </p>
                 </div>
@@ -370,7 +370,7 @@ setloader(true)
                           }
                           setProgresses({});
                         }}
-                        className="w-6 h-6 text-gray-800 dark:text-white"
+                        className="w-6 h-6 text-gray-800"
                         aria-hidden="true"
                         xmlns="http://www.w3.org/2000/svg"
                         width="24"
@@ -412,7 +412,7 @@ setloader(true)
                     inputvideo.value=""
                     setvideoPro({});
                   }}
-                  className="w-6 h-6 text-gray-800 dark:text-white"
+                  className="w-6 h-6 text-gray-800"
                   aria-hidden="true"
                   xmlns="http://www.w3.org/2000/svg"
                   width="24"
@@ -431,11 +431,11 @@ setloader(true)
               </span>
               <label
                 for="dropzone-file"
-                className="flex max-w-[483px] mx-h-[256px] w-full  relative overflow-hidden object-cover object-center flex-col items-center justify-center lg:w-[483px] h-[256px] border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-gray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500"
+                className="flex max-w-[483px] mx-h-[256px] w-full  relative overflow-hidden object-cover object-center flex-col items-center justify-center lg:w-[483px] h-[256px] border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100"
               >
                 <div className="flex flex-col items-center justify-center pt-5 pb-6">
                   <svg
-                    className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400"
+                    className="w-8 h-8 mb-4 text-gray-500"
                     aria-hidden="true"
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
@@ -453,12 +453,12 @@ setloader(true)
                     <source src={videoUrl} type="video/mp4" />
                     Your browser does not support the video tag.
                   </video> : null}
-                  {videoPro.pro != 100 ? <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+                  {videoPro.pro != 100 ? <p className="mb-2 text-sm text-gray-500">
                     <span className="font-semibold">Click to upload</span> or
                     drag and drop
                   </p> : null}
-                   <p className="text-xs text-gray-500 dark:text-gray-400">
-                    SVG, PNG, JPG or GIF (MAX. 800x400px)
+                   <p className="text-xs text-gray-500">
+                    mp4,mkv (MAX. 1920x1080px)
                   </p> 
                 </div>
                 <input
@@ -481,7 +481,7 @@ setloader(true)
 
         <button onClick={()=>{submitDetails()}}
           type="submit"
-          className="text-white mt-6 justify-center flex items-center bg-blue-700 hover:bg-blue-800 w-fit focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+          className="text-white mt-6 justify-center flex items-center bg-[#d42eeb] w-fit focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 focus:outline-none"
         >
           <svg
             className="w-3.5 h-3.5 me-2.5"
