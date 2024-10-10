7ba810c02f7b9096d6d5194cf36c95ba5c04e28d
Aman <160247525+OneNotSeven@users.noreply.github.com>
2024-10-10 21:04:58 +0530

    Update page.js

--- src/app/(home)/courses/page.js
+++ src/app/(home)/courses/page.js
@@ -1,12 +1,23 @@
-
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
 import Cards from '@/app/_Components/Cards'
+import Enrolledcard from '@/app/_Components/Enrolledcard'
 
 
 const page = () => {
+  const [change, setchange] = useState("")
+  
   return (
-    <div className=' w-full h-screen p-4'>
-      <Cards />
+    <div className=' w-full h-full p-4'>
+      <div>
+
+      <select className=' float-end w-28 px-2 font-[Poppins] border-0' onChange={(e)=>setchange(e.target.value)}>
+        <option selected value="Currently">Currently</option>
+        <option value="Enrolled">Enrolled</option>
+      </select>
+      </div>
+      {change != "Enrolled" && <Cards />}
+      {change == "Enrolled" && <Enrolledcard />}
       
     </div>
   )
