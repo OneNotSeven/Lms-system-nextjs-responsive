b196438f6983d154d97676d0e72b44434cfe0cde
Aman <160247525+OneNotSeven@users.noreply.github.com>
2024-11-24 17:51:05 +0530

    Update Sidebar.js

--- src/app/_Components/Sidebar.js
+++ src/app/_Components/Sidebar.js
@@ -56,13 +56,13 @@ const Sidebar = () => {
 
     return (
         <>
-            <div className='sticky font-[Poppins] h-screen hover:text-[#d42eeb] z-20'>
-                <aside id="logo-sidebar" className=" left-0 z-40 w-64 h-screen bg-white border-r border-gray-200 pt-8 transition-transform -translate-x-full bg-white  lg:translate-x-0 fixed top-[64px] lg:sticky lg:block dark:border-gray-700" aria-label="Sidebar">
+            <div className='sticky font-[Poppins] mx hover:text-[#d42eeb] z-20'>
+                <aside id="logo-sidebar" className=" left-0 z-40 w-64 h-screen pt-8 transition-transform -translate-x-full bg-white border-r border-gray-200 lg:translate-x-0 fixed top-[64px] lg:sticky lg:block dark:border-gray-700" aria-label="Sidebar">
                     <div className="h-full px-3 pb-4 relative overflow-y-auto bg-white ">
                         <ul className="space-y-2 font-medium">
                             {/* Sidebar Links */}
                             <li className='group'>
-                                <Link href="/stdashboard" className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100  group">
+                                <Link href="/stdashboard" className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700 group">
                                     <svg className="w-5 h-5 group-hover:fill-[#d42eeb] group-hover:text-[#d42eeb] text-gray-800 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 6.025A7.5 7.5 0 1 0 17.975 14H10V6.025Z"/>
                                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.5 3c-.169 0-.334.014-.5.025V11h7.975c.011-.166.025-.331.025-.5A7.5 7.5 0 0 0 13.5 3Z"/>
@@ -71,7 +71,7 @@ const Sidebar = () => {
                                 </Link>
                             </li>
                             <li className='group'>
-                                <Link href="/courses" className="flex items-center p-2 text-gray-900 rounded-lg  hover:bg-gray-100 group">
+                                <Link href="/courses" className="flex items-center p-2 text-gray-900 rounded-lg  hover:bg-gray-100 dark:hover:bg-gray-700 group">
                                     <svg className="w-5 h-5 group-hover:fill-[#d42eeb] group-hover:text-[#d42eeb] text-gray-800 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9.143 4H4.857A.857.857 0 0 0 4 4.857v4.286c0 .473.384.857.857.857h4.286A.857.857 0 0 0 10 9.143V4.857A.857.857 0 0 0 9.143 4Zm10 0h-4.286a.857.857 0 0 0-.857.857v4.286c0 .473.384.857.857.857h4.286A.857.857 0 0 0 20 9.143V4.857A.857.857 0 0 0 19.143 4Zm-10 10H4.857a.857.857 0 0 0-.857.857v4.286c0 .473.384.857.857.857h4.286a.857.857 0 0 0 .857-.857v-4.286A.857.857 0 0 0 9.143 14Zm10 0h-4.286a.857.857 0 0 0-.857.857v4.286c0 .473.384.857.857.857h4.286a.857.857 0 0 0 .857-.857v-4.286a.857.857 0 0 0-.857-.857Z"/>
                                     </svg>
@@ -79,7 +79,7 @@ const Sidebar = () => {
                                 </Link>
                             </li>
                             <li className='group'>
-                                <Link href="/stdashboard/updatesbytutor" className="flex items-center p-2 text-gray-900 rounded-lg  hover:bg-gray-100 group">
+                                <Link href="/stdashboard/updatesbytutor" className="flex items-center p-2 text-gray-900 rounded-lg  hover:bg-gray-100 dark:hover:bg-gray-700 group">
                                     <svg className="w-5 h-5 group-hover:fill-[#d42eeb] group-hover:text-[#d42eeb] text-gray-800 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7.556 8.5h8m-8 3.5H12m7.111-7H4.89a.896.896 0 0 0-.629.256.868.868 0 0 0-.26.619v9.25c0 .232.094.455.26.619A.896.896 0 0 0 4.89 16H9l3 4 3-4h4.111a.896.896 0 0 0 .629-.256.868.868 0 0 0 .26-.619v-9.25a.868.868 0 0 0-.26-.619.896.896 0 0 0-.63-.256Z"/>
                                     </svg>
@@ -88,7 +88,7 @@ const Sidebar = () => {
                                 </Link>
                             </li>
                             <li className='group'>
-                                <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg  hover:bg-gray-100 group">
+                                <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg  hover:bg-gray-100 dark:hover:bg-gray-700 group">
                                 <svg class="w-5 h-5 group-hover:fill-[#d42eeb] group-hover:text-[#d42eeb] text-gray-800 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
   <path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M4.5 17H4a1 1 0 0 1-1-1 3 3 0 0 1 3-3h1m0-3.05A2.5 2.5 0 1 1 9 5.5M19.5 17h.5a1 1 0 0 0 1-1 3 3 0 0 0-3-3h-1m0-3.05a2.5 2.5 0 1 0-2-4.45m.5 13.5h-7a1 1 0 0 1-1-1 3 3 0 0 1 3-3h3a3 3 0 0 1 3 3 1 1 0 0 1-1 1Zm-1-9.5a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0Z"/>
 </svg>
@@ -111,7 +111,7 @@ const Sidebar = () => {
                                 </li>
 
                                 <li>
-                                    <a onClick={() => setIsModalOpen(true)} href="#" className="flex items-center p-2 text-gray-900 rounded-lg  hover:bg-gray-100 group cursor-pointer">
+                                    <a onClick={() => setIsModalOpen(true)} href="#" className="flex items-center p-2 text-gray-900 rounded-lg  hover:bg-gray-100 dark:hover:bg-gray-700 group cursor-pointer">
                                         <svg className="w-5 h-5 text-gray-800 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 12H4m12 0-4 4m4-4-4-4m3-4h2a3 3 0 0 1 3 3v10a3 3 0 0 1-3 3h-2"/>
                                         </svg>
@@ -127,7 +127,7 @@ const Sidebar = () => {
             {/* Modal */}
             {isModalOpen && (
                 <div className="fixed z-50 inset-0 flex items-center justify-center bg-gray-900 bg-opacity-50">
-                    <div className="bg-white p-6 rounded-lg shadow-lg w-full mx-2 md:w-1/3">
+                    <div className="bg-white p-6 rounded-lg shadow-lg w-1/3">
                         <h3 className="text-lg font-semibold mb-4">Confirm Log Out</h3>
                         <p className="mb-4">Are you sure you want to log out?</p>
                         <div className="flex justify-end space-x-4">
